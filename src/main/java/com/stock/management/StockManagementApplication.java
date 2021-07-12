800adfd6646211fd5636b7bdf1073d23d1141302
Zakaria Sadek <zakariasade20@gmail.com>
2021-07-12 22:16:03 +0100

    create project

---
+++ src/main/java/com/stock/management/StockManagementApplication.java
@@ -0,0 +1,13 @@
+package com.stock.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementApplication.class, args);
+	}
+
+}
